f00395141ecf9904a9cdeec2634cab256c896e74
mardiana1 <mardiana@gmail.com>
2022-02-20 14:48:47 +0700

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div class=' container shadow-md mx-auto h-full bg-white dark:bg-slate-900'>
+  <Header/>
+</div>
+</template>
+
+<script>
+import './index.css';
+import Header from './components/header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-white dark:bg-gray-700 text-gray-700 dark:text-white relative">
+        <img src="../assets/profile.png" class="mx-auto h-56 w-56 rounded-full"/>
+        <img src="../assets/moon.png" class=" absolute top-2 right-4 h-8 w-8 " />
+        <h1 class="font-extrabold text-2xl">Ahmad Mardiana</h1>
+        <span class=" pr-2">twitter</span>
+        <span class=" pr-2">github</span>
+        <span class=" pr-2">linkedin</span>
+    </div>
+</template>
+<script>
+export default {
+  name: 'header',
+  props: {},
+};
+</script>
